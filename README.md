caae5ae6d26cd22c3e90e47f479f8c8d32d3f449
unknown <emailerdinesh@gmail.com>
2018-06-26 14:01:05 +0530

    formattiing issues fixed

--- README.md
+++ README.md
@@ -1,15 +1,53 @@
-# React Nodemailer -Form Emailer
-The project implements a reactstrap based form in the frontend and nodemailer mailing system in the backend
+# React-Express-Nodemailer
 
+The project implements a react-express application that enables a user to send form data as an email to a predefined email address.The mailing functionality is acheived using nodemailer a library which enables us to send emails from our application.
 
-### Getting Started
+## Concepts
 
-Checkout this repo, install dependencies, then start the gulp process with the following. For implementation explanation read the code comment:
+**1.React form**  
+**2.Express Server**  
+**3.Email functionality using Nodemailer**
+
+## Getting Started
+
+There are two methods for getting started with this repo.
+
+**if you are familar with git:**  
+Checkout this repo,install server and client dependancies dependencies, then start the gulp process with the following:
+as follows
 
 ```
-Client folder
-> npm install
-Project folder
-> npm install
-> npm run dev 
+git clone https://github.com/DineshBabu1989/Redux-WeatherApp.git
+cd React-email
+npm install
+cd React-email/client
+npm install
 ```
+
+**if you are not familar with git:**  
+Click here then download the .zip file. Extract the contents of the zip file, then open your terminal,
+change to the project directory, and:
+
+```
+npm install
+npm start
+```
+
+## Testing the app
+
+-field1: city name
+-field2: country code in iso (eg: india -in ,usa -us .etc)
+
+## Prerequisities
+
+-You need to have node.js installed in your system.
+--URL: https://nodejs.org/en/
+
+## Authors
+
+- DINESH.B - FULL STACK DEVELOPER - GEM ENTERPRISES,COIMBATORE,INDIA
+- RAJESH.B - FULL STACK DEVELOPER - ADIKTEEV ,BERLIN,GERMANY
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details.
--- index.js
+++ index.js
@@ -1,57 +1,51 @@
-// server side 
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/api/form',(req,res) => {
-    console.log(req.body);
-    
-var emailcontent = `<h3> Contact Details</h3>
+app.post("/api/form", (req, res) => {
+  console.log(req.body);
+  //Formating content to be send
+  var emailcontent = `<h3> Contact Details</h3>
                      <ul>
                       <li>name: ${req.body.name}</li>
                       <li>email : ${req.body.email}</li>
                      </ul>
                      <h2>Message</h2>
                       <p>message: ${req.body.subject}</p>
-                          `    
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'user@gmail.com',
-    pass: 'your password'
-  },
+                          `;
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "emailerdinesh@gmail.com",
+      pass: "07March1991bro"
+    },
     tls: {
-        rejectUnauthorized: false
+      rejectUnauthorized: false
     }
-
+  });
+  //Preparing the mailOptions object
+  var mailOptions = {
+    from: "emailerdinesh@gmail.com",
+    to: "emailerdinesh@gmail.com",
+    subject: "New Message",
+    text: req.body.subject,
+    html: emailcontent
+  };
+  //Sending email using transporter function
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
 });
-
-var mailOptions = {
-  from: 'emailerdinesh@gmail.com',
-  to: 'emailerdinesh@gmail.com',
-  subject: 'New Message',
-  text: req.body.subject,
-  html: emailcontent
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-    
-});
-
 const PORT = process.env.PORT || 3001;
-
 app.listen(PORT, () => {
-    console.log(`server listening on port ${PORT}`);
+  console.log(`server listening on port ${PORT}`);
 });
